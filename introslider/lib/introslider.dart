c05cdd4383d602f72de9285c9864d011e1bb0904
Abdullah <abdullahsoomro245@gmail.com>
2022-08-06 15:25:38 +0500

    first commit

---
+++ introslider/lib/introslider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+
+class onboardingView extends StatelessWidget {
+  const onboardingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroSlider(
+      
+    );
+  }
+}
